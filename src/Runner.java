7dba8f2338397fdd1f6a4dba140ac8435b1cb93e
BT_1E10_13 <lucyc1225xx@gmail.com>
2022-02-04 11:26:20 -0500

    test

--- src/Runner.java
+++ src/Runner.java
@@ -2,5 +2,6 @@ public class Runner {
     public static void main(String[] args){
         Thing t = new Thing(8);
         System.out.println(t.getNum());
+        System.out.println("Hello");
     }
 }
